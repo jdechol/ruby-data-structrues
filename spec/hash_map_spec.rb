af983d157f9f138ce9b07fc3af2eb8e77ffa39ca
Jared Echols <jechols@mavenlink.com>
2020-09-13 14:55:26 -0600

    Add hashmap with basic functionality

---
+++ spec/hash_map_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe HashMap do
+  describe '#put' do
+    subject { described_class.new }
+
+    before do
+      10.times do |index|
+        subject.put(index, index.to_s)
+      end
+    end
+
+    it 'can update existing values in the hashmap' do
+      subject.put(1, 'one')
+      subject.put(5, 'five')
+      expect(subject.get(1)).to eq 'one'
+      expect(subject.get(5)).to eq 'five'
+    end
+  end
+end
